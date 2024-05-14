64805e74fb938bdab2837581f7d6a747d916dca8
rybik <endyse70@gmail.com>
2024-05-14 16:39:17 +0300

    implemented the test functionality of getting data for displaying dashboards

--- src/pages/ReportsPage/index.tsx
+++ src/pages/ReportsPage/index.tsx
@@ -1,24 +1,39 @@
 import styles from "./ReportsPage.module.scss";
 import { Dashboard } from "superset-dashboard-sdk";
+import {
+  useGetDashboardsUIDQuery,
+  useGetDashboardURLQuery,
+  useGetGuestTokenQuery,
+} from "store/api.ts";
+import SpinLoader from "components/common/SpinLoader";
 // import dp from "./dataProvider";
 
 const Index = () => {
-	return 	(
-		<div>
-			<h1>Отчеты</h1>
-			<div className={styles.container}>
-				<Dashboard
-					uuid="b3df309f-2211-43dc-b519-af3327dcb9e1"
-					domain="http://172.17.53.15:8088"
-					guestToken="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJuYW1lIjoiYWRtaW4iLCJmaXJzdF9uYW1lIjoiYWRtaW4iLCJsYXN0X25hbWUiOiJhZG1pbiJ9LCJyZXNvdXJjZXMiOlt7InR5cGUiOiJkYXNoYm9hcmQiLCJpZCI6ImIzZGYzMDlmLTIyMTEtNDNkYy1iNTE5LWFmMzMyN2RjYjllMSJ9XSwicmxzX3J1bGVzIjpbeyJkYXRhc2V0IjowLCJjbGF1c2UiOiIifV0sImlhdCI6MTcxMjkxNTQ4NS45NzAyNTY4LCJleHAiOjE3MTI5MTU3ODUuOTcwMjU2OCwiYXVkIjoiaHR0cDovLzAuMC4wLjA6ODA4MC8iLCJ0eXBlIjoiZ3Vlc3QifQ.uYTfs414JlSsjinN7L9RAPbu0vRJd8ZvSSW1wwiNxyE"
-				/>
-				{/* <iframe
-					title="sales"
-					src="http://172.17.53.15:8088/superset/dashboard/Sales/?standalone=1&show_filters=1&expand_filters=0"
-				/> */}
-			</div>
-		</div>
-	);
+  const { data: dashboardURL, isLoading: urlIsLoading } =
+    useGetDashboardURLQuery({});
+
+  const { data: dashboardsUID, isLoading: dashboardsUIDIsLoading } =
+    useGetDashboardsUIDQuery({});
+
+  const { data: guestToken, isLoading: guestTokenIsLoading } =
+    useGetGuestTokenQuery({});
+
+  if (urlIsLoading && dashboardsUIDIsLoading && guestTokenIsLoading) {
+    return <SpinLoader />;
+  }
+
+  return (
+    <div>
+      <h1>Отчеты</h1>
+      <div className={styles.container}>
+        <Dashboard
+          uuid={dashboardsUID}
+          domain={dashboardURL}
+          guestToken={guestToken}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Index;
--- src/store/api.ts
+++ src/store/api.ts
@@ -1,134 +1,155 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseUrl = "";
+const baseUrl: string = "ds";
 
 export const queueApi = createApi({
-	reducerPath: "mdlpAPI",
-	tagTypes: ["Users", "Queue"],
-	baseQuery: fetchBaseQuery({ baseUrl }),
-	endpoints: (builder) => ({
-		getQueueAll: builder.query({
-			providesTags: (result) =>
-				result
-					? [
-							...result.map((id: number) => ({
-								type: "Queue",
-								id,
-							})),
-							{ type: "Queue", id: "List" },
-					]
-					: [{ type: "Queue", id: "List" }],
-			query: () => "api/queue/all",
-		}),
-		addQueue: builder.mutation({
-			invalidatesTags: [{ type: "Queue", id: "List" }],
-			query: (data) => ({
-				url: "/api/queue/new",
-				method: "POST",
-				body: data,
-			}),
-		}),
-		deleteTaskFromQueue: builder.mutation({
-			invalidatesTags: [{ type: "Queue", id: "List" }],
-			query: (id: number) => ({
-				url: `/api/queue/delete?id=${id}`,
-				method: "DELETE",
-			}),
-		}),
-		login: builder.mutation({
-			query: ({ username, password }) => ({
-				url: "/login",
-				method: "POST",
-				mode: "no-cors",
-				credentials: "include",
-				body: { username, password },
-			}),
-		}),
-		getUserByUsername: builder.query({
-			providesTags: ["Users"],
-			query: (username: string) => ({
-				url: `api/users/username?username=${username}`,
-				method: "GET",
-			}),
-		}),
-		getUsersAll: builder.query({
-			providesTags: (result) =>
-				result
-					? [
-							...result.map((id: number) => ({
-								type: "Users",
-								id,
-							})),
-							{ type: "Users", id: "List" },
-					]
-					: [{ type: "Users", id: "List" }],
-			query: () => ({
-				url: "api/users/all",
-				method: "GET",
-			}),
-		}),
-		addUser: builder.mutation({
-			invalidatesTags: [{ type: "Users", id: "List" }],
-			query: ({ username, fullName, password }) => ({
-				url: "api/users/add",
-				method: "POST",
-				body: {
-					username,
-					fullName,
-					password,
-					role: "ROLE_USER",
-				},
-			}),
-		}),
-		editUser: builder.mutation({
-			invalidatesTags: [{ type: "Users", id: "List" }],
-			query: (body) => ({
-				url: `api/users/edit`,
-				method: "POST",
-				body,
-			}),
-		}),
-		deleteUser: builder.mutation({
-			invalidatesTags: [{ type: "Users", id: "List" }],
-			query: (id: number) => ({
-				url: `api/users/delete/${id}`,
-				method: "GET",
-			}),
-		}),
-		toggleBlockUser: builder.mutation({
-			invalidatesTags: [{ type: "Users", id: "List" }],
-			query: (id: number) => ({
-				url: `api/users/block/${id}`,
-				method: "GET",
-			}),
-		}),
-		recoveryUser: builder.mutation({
-			invalidatesTags: [{ type: "Users", id: "List" }],
-			query: (id: number) => ({
-				url: `api/users/recovery/${id}`,
-				method: "GET",
-			}),
-		}),
-		getAllCompany: builder.query({
-			query: () => ({
-				url: "api/company/get",
-				method: "GET",
-			}),
-		}),
-	}),
+  reducerPath: "mdlpAPI",
+  tagTypes: ["Users", "Queue"],
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getDashboardURL: builder.query({
+      query: () => ({
+        url: "api/superset/metadata",
+        method: "GET",
+      }),
+    }),
+    getDashboardsUID: builder.query({
+      query: () => ({
+        url: "api/superset/dashboards",
+        method: "GET",
+      }),
+    }),
+    getGuestToken: builder.query({
+      query: () => ({
+        url: "api/superset/guest-token",
+        method: "GET",
+      }),
+    }),
+    getQueueAll: builder.query({
+      providesTags: (result) =>
+        result
+          ? [
+              ...result.map((id: number) => ({
+                type: "Queue",
+                id,
+              })),
+              { type: "Queue", id: "List" },
+            ]
+          : [{ type: "Queue", id: "List" }],
+      query: () => "api/queue/all",
+    }),
+    addQueue: builder.mutation({
+      invalidatesTags: [{ type: "Queue", id: "List" }],
+      query: (data) => ({
+        url: "/api/queue/new",
+        method: "POST",
+        body: data,
+      }),
+    }),
+    deleteTaskFromQueue: builder.mutation({
+      invalidatesTags: [{ type: "Queue", id: "List" }],
+      query: (id: number) => ({
+        url: `/api/queue/delete?id=${id}`,
+        method: "DELETE",
+      }),
+    }),
+    login: builder.mutation({
+      query: ({ username, password }) => ({
+        url: "/login",
+        method: "POST",
+        mode: "no-cors",
+        credentials: "include",
+        body: { username, password },
+      }),
+    }),
+    getUserByUsername: builder.query({
+      providesTags: ["Users"],
+      query: (username: string) => ({
+        url: `api/users/username?username=${username}`,
+        method: "GET",
+      }),
+    }),
+    getUsersAll: builder.query({
+      providesTags: (result) =>
+        result
+          ? [
+              ...result.map((id: number) => ({
+                type: "Users",
+                id,
+              })),
+              { type: "Users", id: "List" },
+            ]
+          : [{ type: "Users", id: "List" }],
+      query: () => ({
+        url: "api/users/all",
+        method: "GET",
+      }),
+    }),
+    addUser: builder.mutation({
+      invalidatesTags: [{ type: "Users", id: "List" }],
+      query: ({ username, fullName, password }) => ({
+        url: "api/users/add",
+        method: "POST",
+        body: {
+          username,
+          fullName,
+          password,
+          role: "ROLE_USER",
+        },
+      }),
+    }),
+    editUser: builder.mutation({
+      invalidatesTags: [{ type: "Users", id: "List" }],
+      query: (body) => ({
+        url: `api/users/edit`,
+        method: "POST",
+        body,
+      }),
+    }),
+    deleteUser: builder.mutation({
+      invalidatesTags: [{ type: "Users", id: "List" }],
+      query: (id: number) => ({
+        url: `api/users/delete/${id}`,
+        method: "GET",
+      }),
+    }),
+    toggleBlockUser: builder.mutation({
+      invalidatesTags: [{ type: "Users", id: "List" }],
+      query: (id: number) => ({
+        url: `api/users/block/${id}`,
+        method: "GET",
+      }),
+    }),
+    recoveryUser: builder.mutation({
+      invalidatesTags: [{ type: "Users", id: "List" }],
+      query: (id: number) => ({
+        url: `api/users/recovery/${id}`,
+        method: "GET",
+      }),
+    }),
+    getAllCompany: builder.query({
+      query: () => ({
+        url: "api/company/get",
+        method: "GET",
+      }),
+    }),
+  }),
 });
 
 export const {
-	useAddQueueMutation,
-	useGetAllCompanyQuery,
-	useDeleteTaskFromQueueMutation,
-	useRecoveryUserMutation,
-	useToggleBlockUserMutation,
-	useDeleteUserMutation,
-	useGetQueueAllQuery,
-	useLoginMutation,
-	useGetUserByUsernameQuery,
-	useGetUsersAllQuery,
-	useAddUserMutation,
-	useEditUserMutation,
+  useGetDashboardsUIDQuery,
+  useGetGuestTokenQuery,
+  useGetDashboardURLQuery,
+  useAddQueueMutation,
+  useGetAllCompanyQuery,
+  useDeleteTaskFromQueueMutation,
+  useRecoveryUserMutation,
+  useToggleBlockUserMutation,
+  useDeleteUserMutation,
+  useGetQueueAllQuery,
+  useLoginMutation,
+  useGetUserByUsernameQuery,
+  useGetUsersAllQuery,
+  useAddUserMutation,
+  useEditUserMutation,
 } = queueApi;
