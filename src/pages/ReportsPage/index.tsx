abcc56cab41970b29cfa92368256ef2676a95cd2
Rybik52 <endyse70@gmail.com>
2024-05-14 17:44:29 +0300

    fix baseUrl and props for Dashboard component

--- src/pages/ReportsPage/index.tsx
+++ src/pages/ReportsPage/index.tsx
@@ -1,39 +1,40 @@
 import styles from "./ReportsPage.module.scss";
 import { Dashboard } from "superset-dashboard-sdk";
 import {
-  useGetDashboardsUIDQuery,
-  useGetDashboardURLQuery,
-  useGetGuestTokenQuery,
+	useGetDashboardsUIDQuery,
+	useGetDashboardURLQuery,
+	useGetGuestTokenQuery,
 } from "store/api.ts";
 import SpinLoader from "components/common/SpinLoader";
 // import dp from "./dataProvider";
 
 const Index = () => {
-  const { data: dashboardURL, isLoading: urlIsLoading } =
-    useGetDashboardURLQuery({});
+	const { data: dashboardURL, isLoading: urlLoading } =
+		useGetDashboardURLQuery({});
 
-  const { data: dashboardsUID, isLoading: dashboardsUIDIsLoading } =
-    useGetDashboardsUIDQuery({});
+	const { data: dashboardsUID, isLoading: uidLoading } =
+		useGetDashboardsUIDQuery({});
 
-  const { data: guestToken, isLoading: guestTokenIsLoading } =
-    useGetGuestTokenQuery({});
+	const { data: guestToken, isLoading: tokenLoading } = useGetGuestTokenQuery(
+		{}
+	);
 
-  if (urlIsLoading && dashboardsUIDIsLoading && guestTokenIsLoading) {
-    return <SpinLoader />;
-  }
+	if (urlLoading || uidLoading || tokenLoading) {
+		return <SpinLoader />;
+	}
 
-  return (
-    <div>
-      <h1>Отчеты</h1>
-      <div className={styles.container}>
-        <Dashboard
-          uuid={dashboardsUID}
-          domain={dashboardURL}
-          guestToken={guestToken}
-        />
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Отчеты</h1>
+			<div className={styles.container}>
+				<Dashboard
+					uuid={dashboardsUID[0]}
+					domain={dashboardURL?.host}
+					guestToken={guestToken?.token}
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default Index;
